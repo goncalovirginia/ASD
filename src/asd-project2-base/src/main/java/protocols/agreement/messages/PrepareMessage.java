b3cdb9b297966499de0c15d35ac57d2adf15cf6b
ricardo rodrigues <ricardo.rodrigues@banetech.net>
2024-11-07 02:11:18 +0000

    first commit

---
+++ src/asd-project2-base/src/main/java/protocols/agreement/messages/PrepareMessage.java
@@ -0,0 +1,72 @@
+package protocols.agreement.messages;
+
+import io.netty.buffer.ByteBuf;
+import org.apache.commons.codec.binary.Hex;
+import pt.unl.fct.di.novasys.babel.generic.ProtoMessage;
+import pt.unl.fct.di.novasys.network.ISerializer;
+
+import java.util.UUID;
+
+/*************************************************
+ * This is here just as an example, your solution
+ * probably needs to use different message types
+ *************************************************/
+public class PrepareMessage extends ProtoMessage {
+
+    public final static short MSG_ID = 106;
+
+    private final UUID opId;
+    private final int instance;
+    private final byte[] op;
+
+    public PrepareMessage(int instance, UUID opId, byte[] op) {
+        super(MSG_ID);
+        this.instance = instance;
+        this.op = op;
+        this.opId = opId;
+    }
+
+    public int getInstance() {
+        return instance;
+    }
+
+    public UUID getOpId() {
+        return opId;
+    }
+
+    public byte[] getOp() {
+        return op;
+    }
+
+    @Override
+    public String toString() {
+        return "PrepareMessage{" +
+                "opId=" + opId +
+                ", instance=" + instance +
+                ", op=" + Hex.encodeHexString(op) +
+                '}';
+    }
+
+    public static ISerializer<PrepareMessage> serializer = new ISerializer<PrepareMessage>() {
+        @Override
+        public void serialize(PrepareMessage msg, ByteBuf out) {
+            out.writeInt(msg.instance);
+            out.writeLong(msg.opId.getMostSignificantBits());
+            out.writeLong(msg.opId.getLeastSignificantBits());
+            out.writeInt(msg.op.length);
+            out.writeBytes(msg.op);
+        }
+
+        @Override
+        public PrepareMessage deserialize(ByteBuf in) {
+            int instance = in.readInt();
+            long highBytes = in.readLong();
+            long lowBytes = in.readLong();
+            UUID opId = new UUID(highBytes, lowBytes);
+            byte[] op = new byte[in.readInt()];
+            in.readBytes(op);
+            return new PrepareMessage(instance, opId, op);
+        }
+    };
+
+}
